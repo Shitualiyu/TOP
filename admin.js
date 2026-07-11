8667001de0d4e5f4849f88279d4cb3633bd010fe
Shitualiyu <aliyuashitu788@gmail.com>
2026-07-11 02:23:44 +0100

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,23 @@
+import { auth } from "./firebase.js";
+
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const adminEmail = "aliyuashitu788@gmail.com";
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    if (user.email !== adminEmail) {
+        alert("Access Denied");
+        window.location.href = "dashboard.html";
+        return;
+    }
+
+    document.getElementById("depositList").innerHTML =
+        "<h3>✅ Welcome Admin</h3>";
+
+});
