79ad062740af22367d75bc93df5b97e4a9a27045
Shitualiyu <aliyuashitu788@gmail.com>
2026-07-11 01:29:30 +0100

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,33 +1,40 @@
-alert("deposit.js loaded");
 import { auth, db } from "./firebase.js";
-
 import { addDoc, collection } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 document.getElementById("depositBtn").addEventListener("click", async () => {
 
-    const user = auth.currentUser;
+    try {
 
-    if (!user) {
-        alert("Please login first.");
-        return;
+        const user = auth.currentUser;
+
+        if (!user) {
+            alert("Please login first.");
+            return;
+        }
+
+        const amount = Number(document.getElementById("amount").value);
+        const method = document.getElementById("method").value;
+
+        if (!amount || amount <= 0) {
+            alert("Enter a valid amount.");
+            return;
+        }
+
+        await addDoc(collection(db, "depositRequests"), {
+            userId: user.uid,
+            email: user.email,
+            amount: amount,
+            method: method,
+            status: "Pending",
+            createdAt: new Date().toISOString()
+        });
+
+        document.getElementById("status").innerText =
+            "✅ Deposit request submitted successfully!";
+
+    } catch (error) {
+        alert(error.message);
+        console.log(error);
     }
 
-    const amount = Number(document.getElementById("amount").value);
-    const method = document.getElementById("method").value;
-
-    if (!amount || amount <= 0) {
-        alert("Enter a valid amount.");
-        return;
-    }
-
-    await addDoc(collection(db, "depositRequests"), {
-    userId: user.uid,
-    email: user.email,
-    amount: amount,
-    method: method,
-    status: "Pending",
-    createdAt: new Date().toISOString()
-});
-    document.getElementById("status").innerText =
-        "✅ Deposit request submitted successfully!";
 });
