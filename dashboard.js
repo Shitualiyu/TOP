96b8e93421a0104cbf5b840ecbc969394b27aef1
Shitualiyu <aliyuashitu788@gmail.com>
2026-07-11 18:07:21 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,18 @@
 import { auth, db } from "./firebase.js";
 
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { 
+    onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import {
+    doc,
+    getDoc,
+    collection,
+    query,
+    where,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 
 onAuthStateChanged(auth, async (user) => {
 
@@ -11,18 +21,77 @@ onAuthStateChanged(auth, async (user) => {
         return;
     }
 
-    const docRef = doc(db, "users", user.uid);
-    const docSnap = await getDoc(docRef);
 
-   if (docSnap.exists()) {
+    // Load user data
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
 
-    const data = docSnap.data();
 
-    document.getElementById("welcomeUser").innerText =
-        "Welcome, " + data.fullname + " 👋";
+    if (userSnap.exists()) {
 
-    document.getElementById("balance").innerText =
-        "₦" + Number(data.balance).toLocaleString();
+        const data = userSnap.data();
 
-}
+        document.getElementById("welcomeUser").innerText =
+            "Welcome, " + data.fullname + " 👋";
+
+
+        document.getElementById("balance").innerText =
+            "₦" + Number(data.balance).toLocaleString();
+
+    }
+
+
+
+    // ==========================
+    // Transaction History
+    // ==========================
+
+    const transactionList = document.getElementById("transactionList");
+
+    transactionList.innerHTML = "<h3>Transaction History</h3>";
+
+
+    const depositQuery = query(
+        collection(db, "depositRequests"),
+        where("userId", "==", user.uid)
+    );
+
+
+    const depositSnapshot = await getDocs(depositQuery);
+
+
+    if (depositSnapshot.empty) {
+
+        transactionList.innerHTML += 
+        "<p>No transactions found.</p>";
+
+        return;
+    }
+
+
+
+    depositSnapshot.forEach((transaction) => {
+
+        const data = transaction.data();
+
+
+        transactionList.innerHTML += `
+
+        <div class="transaction-card">
+
+            <p><b>Type:</b> Deposit</p>
+
+            <p><b>Amount:</b> ₦${data.amount}</p>
+
+            <p><b>Method:</b> ${data.method}</p>
+
+            <p><b>Status:</b> ${data.status}</p>
+
+        </div>
+
+        `;
+
+    });
+
+
 });
